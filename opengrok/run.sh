9b2101694628ee31c499d0fdf8416d92015194e6
koooge <koooooge@gmail.com>
2017-01-01 15:18:42 +0100

    * opengrok/run.sh: modify root path

--- opengrok/run.sh
+++ opengrok/run.sh
@@ -3,12 +3,12 @@
 /opengrok/bin/OpenGrok deploy
 
 service tomcat7 start
-svn co https://svn.ruby-lang.org/repos/ruby/trunk /src
+svn co https://svn.ruby-lang.org/repos/ruby/trunk /src/trunk
 /opengrok/bin/OpenGrok index
 
 while true
 do
   sleep 43200
-  svn update /src
+  svn update /src/trunk
   /opengrok/bin/OpenGrok index
 done
